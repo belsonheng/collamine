81b5f6928864e48cbdaf7b290f27bf69b3f736fa
Belson <belson@server.local>
2014-10-27 18:09:48 +0800

    fix syntax

--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'domainatrix'
 require 'mongo'
 
 pages, from_collamine = Collamine.start('http://forums.hardwarezone.com.sg/hwm-magazine-publication-38/', 
-	                                    :pattern => Regexp.new('^http:\/\/forums\.hardwarezone\.com\.sg\/hwm-magazine-publication-38\/?(.*\.html)?$'))
+                                        :pattern => Regexp.new('^http:\/\/forums\.hardwarezone\.com\.sg\/hwm-magazine-publication-38\/?(.*\.html)?$'))
 
 
 puts "### Start inserting crawled resutls to database..."
