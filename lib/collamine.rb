561577f067e1fd107b8ae703155ca43601817856
Belson <belson@server.local>
2015-02-10 10:58:52 +0800

    bug fix

--- lib/collamine.rb
+++ lib/collamine.rb
@@ -1,7 +1,13 @@
 require 'collamine/request'
 require 'spidercrawl'
 
+require 'curb'
+require 'domainatrix'
+require 'mongo'
+
 class Collamine
+  include Mongo
+  STORE = MongoClient.new("localhost", 27017).db("smartcache").collection("html")
 
   def self.start(url, options)
     from_collamine = []
@@ -15,12 +21,27 @@ class Collamine
         collamine
       end
       web.after_fetch do |page|
-        # Upload to collamine if it cannot be found in server
-        unless collamine then 
-          puts "uploading to collamine: #{page.url}"
-          filename = page.url.split('/').last
-          filename += '.html' unless filename.include?('.html')
-          Request.upload_to_collamine(page.url, page.content, filename, page.crawled_time.to_i)
+        unless page.content == ''
+          # Upload to collamine if it cannot be found in server
+          unless collamine
+            puts "uploading to collamine: #{page.url}"
+            filename = page.url.split('/').last
+            filename += '.html' unless filename.include?('.html')
+            Request.upload_to_collamine(page.url, page.content, filename, page.crawled_time.to_i)
+          end
+          # Check if duplicate
+          unless STORE.find("url" => page.url).to_a.size > 0
+            # Insert into Mongodb
+            puts "Insert to db: #{page.url}"
+            source = (from_collamine.include?(page.url) ? 'collamine' : 'original')
+            doc = {:url => page.url, 
+                   :domain => Domainatrix.parse(page.url).domain,
+                   :source => source, 
+                   :content => page.content.encode('UTF-8', 'ISO-8859-15'), 
+                   :crawled_date =>  page.crawled_time.to_i,
+                   :response_time => page.response_time.to_i
+            }
+          end
         end
       end
     end
--- test.rb
+++ test.rb
@@ -3,30 +3,6 @@ require 'domainatrix'
 require 'mongo'
 
 pages, from_collamine = Collamine.start('http://forums.hardwarezone.com.sg/hwm-magazine-publication-38/', 
-	                                    :parallel => true,
-	                                    :threads => 30,
-                                        :pattern => Regexp.new('^http:\/\/forums\.hardwarezone\.com\.sg\/hwm-magazine-publication-38\/?(.*\.html)?$'))
-
-
-puts "### Start inserting crawled resutls to database..."
-#Save results to mongodb
-include Mongo
-db = MongoClient.new("localhost", 27017).db("smartcache")
-store = db.collection("html")
-
-pages.sort_by! { |page| page.url }
-
-pages.each do |page| 
-  puts page.url
-  source = (from_collamine.include?(page.url) ? 'collamine' : 'original')
-  doc = {"url" => page.url, 
-         "domain" => Domainatrix.parse(page.url).domain,
-         "source" => source, 
-         "content" => page.content.encode('UTF-8', 'ISO-8859-15'), 
-         "crawled_date" =>  page.crawled_time.to_i,
-         "response_time" => page.response_time.to_i
-        }
-  id = store.insert(doc)
-end
-puts "Total pages: #{pages.size}"
-puts "done"
+	                                     :parallel => true,
+	                                     :threads => 30,
+                                       :pattern => Regexp.new('^http:\/\/forums\.hardwarezone\.com\.sg\/hwm-magazine-publication-38\/?(.*\.html)?$'))
