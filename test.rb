86beb94282e83a0586b3e6276bba748cf290e95a
Belson <belson@server.local>
2015-02-10 10:59:57 +0800

    bug fix

--- test.rb
+++ test.rb
@@ -3,6 +3,6 @@ require 'domainatrix'
 require 'mongo'
 
 pages, from_collamine = Collamine.start('http://forums.hardwarezone.com.sg/hwm-magazine-publication-38/', 
-	                                     :parallel => true,
-	                                     :threads => 30,
+                                       :parallel => true,
+                                       :threads => 30,
                                        :pattern => Regexp.new('^http:\/\/forums\.hardwarezone\.com\.sg\/hwm-magazine-publication-38\/?(.*\.html)?$'))
