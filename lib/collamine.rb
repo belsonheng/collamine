645e8576d06e98810bebd1972083e992c48e4725
Belson <belson@server.local>
2016-03-16 16:05:23 +0800

    remove mongodb dependency and expose before_fetch and after_fetch yield blocks

--- lib/collamine.rb
+++ lib/collamine.rb
@@ -1,19 +1,14 @@
 require 'collamine/request'
 require 'spidercrawl'
 
-require 'curb'
-require 'domainatrix'
-require 'mongo'
-
 class Collamine
-  include Mongo
-  STORE = MongoClient.new("localhost", 27017).db("smartcache").collection("html")
-
   def self.start(url, options)
+    Request.setup_collamine(download: options[:download], upload: options[:upload])
     from_collamine = []
     pages = Spiderman.shoot(url, options) do |web|
       collamine = nil
       web.before_fetch do |url|
+        @setup.yield url unless @setup.nil?
         # Try to fetch from collamine server
         puts "trying collamine: #{url}" 
         puts "fetched from collamine: #{url}" if (collamine = Request.try_collamine(url))
@@ -36,25 +31,18 @@ class Collamine
             filename += '.html' unless filename.include?('.html')
             Request.upload_to_collamine(page.url, page.content, filename, page.crawled_time.to_i)
           end
-          # Check if duplicate
-          unless STORE.find("url" => page.url).to_a.size > 0
-            # Insert into Mongodb
-            puts "Insert to db: #{page.url}"
-            source = (from_collamine.include?(page.url) ? 'collamine' : 'original')
-            doc = {:url => page.url, 
-                   :domain => Domainatrix.parse(page.url).domain,
-                   :source => source, 
-                   :content => page.content.encode('UTF-8', 'ISO-8859-15'), 
-                   :crawled_date =>  page.crawled_time.to_i,
-                   :response_time => page.response_time.to_i
-            }
-            STORE.insert(doc)
-          else
-            puts "url exists"
-          end
         end
+        @teardown.yield page, from_collamine unless @teardown.nil?
       end
     end
     return pages, from_collamine
+  end
+
+  def self.before_fetch(&block)
+    @setup = block if block
+  end
+
+  def self.after_fetch(&block)
+    @teardown = block if block
   end
 end
